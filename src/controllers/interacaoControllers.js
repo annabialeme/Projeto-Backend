6262f1f9c95e002c6987b41b58fedce2f02629d3
anna leme <162629023+annabialeme@users.noreply.github.com>
2025-09-04 16:27:21 -0300

    feat: create getInteracoes

--- src/controllers/interacaoControllers.js
+++ src/controllers/interacaoControllers.js
@@ -15,4 +15,26 @@ const getAllInteracoes = async (requestAnimationFrame, res) => {
             details: err.message
         });
     }
-}
+};
+
+const getInteracoesByPersonagem = async (req, res) => {
+    try {
+        const {personagemId} = req.params;
+        const interacoes = await Interacao.getInteracoesByPersonagem(personagemId);
+
+        if(interacoes.lenght === 0) {
+            return res.status(404).json({error: "Nenhuma interação encontrada para este personagem"});
+        }
+
+        res.status(200).json({
+            message: `Interações do personagem ${personagemId} recuperadas com sucesso`,
+            total: interacoes.lenght,
+            data: interacoes
+        });
+    } catch (err) {
+        console.error("Erro ao buscar interações por personagem:", err);
+        res.status(500).json({
+            error: "Erro interno",
+            details: err.message
+    }
+};

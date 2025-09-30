b064164ebd76b81118d88add832e1f1756ecb70c
anna leme <162629023+annabialeme@users.noreply.github.com>
2025-09-30 11:53:51 -0300

    feat: api key

--- src/routes/detalhesRoutes.js
+++ src/routes/detalhesRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const detalhesController = require("../controllers/detalhesControllers");
 const upload = require("../config/upload");
+const apiKeyMiddleware = require("../config/apiKey"); 
 
 router.get("/", detalhesController.getAllDetalhes);
 router.get("/:id", detalhesController.getDetalheById);
@@ -9,5 +10,5 @@ router.post("/", upload.single("imagem"), detalhesController.createDetalhe);
 router.put("/:id", upload.single("imagem"), detalhesController.updateDetalhe);
 router.delete("/:id", detalhesController.deleteDetalhe);
 
-
+router.use(apiKeyMiddleware);
 module.exports = router;
--- src/routes/personangensRoutes.js
+++ src/routes/personangensRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const personagensController = require("../controllers/personagensController");
 const upload = require("../config/upload");
+const apiKeyMiddleware = require("../config/apiKey"); 
 
 router.post("/", upload.single("imagem_url"), personagensController.createPersonagem);
 router.get("/", personagensController.getAllPersonagens);
@@ -9,4 +10,5 @@ router.get("/:id", personagensController.getPersonagemById);
 router.put("/:id", upload.single("imagem_url"), personagensController.updatePersonagem);
 router.delete("/:id", personagensController.deletePersonagem);
 
+router.use(apiKeyMiddleware);
 module.exports = router;
